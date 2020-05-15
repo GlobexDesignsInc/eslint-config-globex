9381b9aafe426a976e698b0f0c47c8331746292f
Ev Haus <ev@zenhub.com>
2020-05-14 22:29:11 -0700

    fix: specify that import/export is okay for no-unsupported-features

--- rules/node.js
+++ rules/node.js
@@ -21,7 +21,9 @@ module.exports = {
 		'node/no-unpublished-import': 'error',
 		'node/no-unpublished-require': 'error',
 		'node/no-unsupported-features/es-builtins': 'error',
-		'node/no-unsupported-features/es-syntax': 'error',
+		'node/no-unsupported-features/es-syntax': ['error', {
+			ignores: ['modules'],
+		}],
 		'node/no-unsupported-features/node-builtins': 'error',
 		'node/prefer-global/buffer': ['error', 'always'],
 		'node/prefer-global/console': ['error', 'always'],
