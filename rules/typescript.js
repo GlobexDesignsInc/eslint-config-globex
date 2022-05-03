153c9d5ba275be67dc45e2f3564329d363184914
Ev Haus <ev@haus.gg>
2022-05-02 20:46:22 -0700

    feat: add @typescript-eslint/no-duplicate-enum-valuesrule

--- rules/typescript.js
+++ rules/typescript.js
@@ -131,6 +131,7 @@ module.exports = {
 			'@typescript-eslint/no-base-to-string': 'error',
 			'@typescript-eslint/no-confusing-non-null-assertion': 'error',
 			'@typescript-eslint/no-dupe-class-members': 'error',
+			'@typescript-eslint/no-duplicate-enum-values': 'error',
 			'@typescript-eslint/no-duplicate-imports': 'error',
 			'@typescript-eslint/no-dynamic-delete': 'error',
 			'@typescript-eslint/no-empty-interface': 'error',
