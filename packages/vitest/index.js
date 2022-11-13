57a9d57a0883eacfd8c02c9b532c75590ce5f529
Ev Haus <ev@haus.gg>
2022-11-13 12:22:36 -0800

    feat: enable new vitest rules

--- packages/vitest/index.js
+++ packages/vitest/index.js
@@ -3,7 +3,9 @@ module.exports = {
 	rules: {
 		// ERRORS
 
+		'vitest/expect-expect': 'error',
 		'vitest/max-nested-describe': ['error', {max: 2}],
+		'vitest/no-conditional-tests': 'error',
 		'vitest/no-focused-tests': 'error',
 		'vitest/no-identical-title': 'error',
 		'vitest/no-skipped-tests': 'error',
