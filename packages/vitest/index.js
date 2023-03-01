64f780f274b4604e50ae2301c3fd84e2f7747d24
Ev Haus <ev@haus.gg>
2023-02-28 19:47:59 -0800

    feat: enable new vitest rules

--- packages/vitest/index.js
+++ packages/vitest/index.js
@@ -7,7 +7,12 @@ module.exports = {
 		'vitest/consistent-test-it': ['error', {fn: 'it'}],
 		'vitest/expect-expect': 'error',
 		'vitest/max-nested-describe': ['error', {max: 2}],
+		'vitest/no-alias-methods': 'error',
+		'vitest/no-commented-out-tests': 'error',
+		'vitest/no-conditional-expect': 'error',
+		'vitest/no-conditional-in-test': 'error',
 		'vitest/no-conditional-tests': 'error',
+		'vitest/no-disabled-tests': 'error',
 		'vitest/no-focused-tests': 'error',
 		'vitest/no-identical-title': 'error',
 		'vitest/no-skipped-tests': 'error',
@@ -17,6 +22,7 @@ module.exports = {
 
 		// DISABLED
 
+		'vitest/max-expects': 'off',
 		'vitest/no-hooks': 'off',
 		'vitest/no-restricted-vi-methods': 'off',
 		'vitest/prefer-lowercase-title': 'off',
